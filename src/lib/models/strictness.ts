bf2d19fa63080c5af59b41275583f2be7a505d5f
tipsypastels <dakotasankey@gmail.com>
2024-12-16 15:04:44 -0800

    add strictness

---
+++ src/lib/models/strictness.ts
@@ -0,0 +1,32 @@
+export interface Strictness {
+  name: string;
+  description: string;
+  maximumRatioDifference: number;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace Strictness {
+  export const Easygoing: Strictness = {
+    name: "Easygoing",
+    description: "If you're just trying out Stardex.",
+    maximumRatioDifference: 0.1,
+  };
+
+  export const Normal: Strictness = {
+    name: "Normal",
+    description: "If you're using Stardex as a rough guideline.",
+    maximumRatioDifference: 0.04,
+  };
+
+  export const Strict: Strictness = {
+    name: "Strict",
+    description: "If you're using Stardex as a strict guideline.",
+    maximumRatioDifference: 0.02,
+  };
+
+  export const Bitchy: Strictness = {
+    name: "Bitchy",
+    description: "If you care way too much.",
+    maximumRatioDifference: 0.01,
+  };
+}
--- src/lib/state.ts
+++ src/lib/state.ts
@@ -1,15 +1,18 @@
 import { Set as ISet } from "immutable";
 import { derived, writable } from "svelte/store";
-import { resolveSpecies } from "./models/species";
-import { type Pokemon } from "./models/pokemon";
+import type { Pokemon } from "./models/pokemon";
 import { INITIAL_REGION_KEYS, type RegionKey } from "./models/region";
+import { resolveSpecies } from "./models/species";
+import { Strictness } from "./models/strictness";
 
 export interface State {
+  strictness: Strictness;
   regions: ISet<RegionKey>;
   pokemon: Pokemon[];
 }
 
 export const state = writable<State>({
+  strictness: Strictness.Normal,
   regions: ISet(INITIAL_REGION_KEYS),
   pokemon: [{ species: resolveSpecies("bulbasaur") }],
 });
