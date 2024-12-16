6802cafd5258aa8766533887cd9a7eafd77bf340
tipsypastels <dakotasankey@gmail.com>
2024-12-16 11:45:14 -0800

    use adapter-static, whoops

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 import { mdsvex } from "mdsvex";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
