9551b04941e9f03dbac1fac29e4ce0140a0b7e5a
tipsypastels <dakotasankey@gmail.com>
2024-12-16 16:42:39 -0800

    don't forget to use base

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+  import { base } from "$app/paths";
   import "../app.css";
+
   let { children } = $props();
 </script>
+
+<svelte:head>
+  <link href="{base}/fa/css/fontawesome.min.css" rel="stylesheet" />
+  <link href="{base}/fa/css/solid.min.css" rel="stylesheet" />
+</svelte:head>
 
 <main class="m-auto w-[800px] max-w-full">
   {@render children()}
