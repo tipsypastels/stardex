a44a78d15e7bfe0e7b41fae68010d785319848c1
tipsypastels <dakotasankey@gmail.com>
2024-12-18 18:18:12 -0800

    compare page

--- src/routes/breakdown/+page.svelte
+++ src/routes/breakdown/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import IconLink from "$lib/components/common/IconLink.svelte";
   import Layout from "$lib/components/layout/Layout.svelte";
   import Section from "$lib/components/layout/Section.svelte";
   import TypePieChart from "$lib/components/pokemon/util/TypePieChart.svelte";
-  import { pokemonAllotment, recommendations, regionsAllotment } from "$lib/state/metrics";
+  import { pokemonAllotment, recommendations } from "$lib/state/metrics";
+  import { regions } from "$lib/state/regions";
 </script>
 
 <Layout title="Breakdown">
@@ -11,6 +13,12 @@
   </Section>
 
   <Section title="Recommendations">
-    <pre>{JSON.stringify($recommendations, null, 2)}</pre>
+    {#if $regions.size > 0}
+      <pre>{JSON.stringify($recommendations, null, 2)}</pre>
+    {:else}
+      <p>
+        Select some regions on the <IconLink to="settings" /> page to use this feature.
+      </p>
+    {/if}
   </Section>
 </Layout>
--- src/routes/compare/+page.svelte
+++ src/routes/compare/+page.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
+  import IconLink from "$lib/components/common/IconLink.svelte";
   import Layout from "$lib/components/layout/Layout.svelte";
+  import Section from "$lib/components/layout/Section.svelte";
+  import TypePieChart from "$lib/components/pokemon/util/TypePieChart.svelte";
+  import { pokemonAllotment, regionsAllotment } from "$lib/state/metrics";
+  import { regions } from "$lib/state/regions";
 </script>
 
-<Layout title="Compare">with what</Layout>
+<Layout title="Compare">
+  <Section title="Types">
+    <TypePieChart allotment={$pokemonAllotment} />
+  </Section>
+
+  <Section title="Compared Types">
+    {#if $regions.size > 0}
+      <TypePieChart allotment={$regionsAllotment} />
+    {:else}
+      <p>
+        Select some regions on the <IconLink to="settings" /> page to use this feature.
+      </p>
+    {/if}
+  </Section>
+</Layout>
