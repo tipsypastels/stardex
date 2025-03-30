8f43a9dc4bdd649e2ed62dd100e9eaae5cb46217
tipsypastels <dakotasankey@gmail.com>
2025-03-29 20:04:45 -0700

    footer attribution

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,6 +1,11 @@
 <footer class="mb-8 border-t-[1px] border-t-slate-300 pt-8 text-center">
+  <div class="mb-1">
+    Created by <a class="underline hover:text-lime-600" href="https://github.com/tipsypastels"
+      >tipsypastels</a
+    >
+  </div>
   <a
-    class="underline hover:text-lime-600"
+    class="block underline hover:text-lime-600"
     href="https://github.com/tipsypastels/stardex"
     target="_blank">Stardex on GitHub</a
   >
