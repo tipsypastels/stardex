cbcfd7a511ab3aa2695beebbe610ad456997c5b9
tipsypastels <dakotasankey@gmail.com>
2025-03-30 09:10:15 -0700

    add title and label to logo

--- src/lib/components/layout/Logo.svelte
+++ src/lib/components/layout/Logo.svelte
@@ -3,6 +3,8 @@
 </script>
 
 <a
+  title="Stardex"
+  aria-label="Stardex"
   href={PAGE_LINKS.editor.href}
   class="mx-auto mb-4 flex h-[55px] w-[55px] items-center justify-center rounded-md bg-lime-600 text-3xl font-bold text-white shadow-xl shadow-slate-400 md:mx-0 lg:absolute lg:left-4 lg:top-4"
 >
