eebde1b288d4276ad04a48f9aa7eab9e57d14607
tipsypastels <dakotasankey@gmail.com>
2026-05-30 09:22:01 -0700

    hmm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,39 @@
 <script lang="ts">
   import IconLink from "$lib/components/common/IconLink.svelte";
   import Layout from "$lib/components/layout/Layout.svelte";
+  import Logo from "$lib/components/layout/Logo.svelte";
   import Section from "$lib/components/layout/Section.svelte";
   import AddPokemon from "$lib/components/pokemon/add/AddPokemon.svelte";
   import Pokedex from "$lib/components/pokemon/Pokedex.svelte";
   import Recommendations from "$lib/components/pokemon/Recommendations.svelte";
   import TypePieChart from "$lib/components/pokemon/util/TypePieChart.svelte";
+  import ProjectsModal from "$lib/components/projects/ProjectsModal.svelte";
+  import ProjectsSelect from "$lib/components/projects/ProjectsSelect.svelte";
   import { pokemonAllotment } from "$lib/state/metrics";
   import { pokemon } from "$lib/state/pokemon";
   import { regions } from "$lib/state/regions";
 </script>
 
-<Layout title="Editor">
+<div class="mx-4 mt-4 mb-8 flex items-center gap-4 border-b-1 border-b-slate-300 pb-6">
+  <Logo />
+  <ProjectsSelect />
+</div>
+
+<div class="mx-8 flex gap-8">
+  <div class="grow">
+    <AddPokemon />
+    <Pokedex />
+  </div>
+
+  <div class="grow">
+    <TypePieChart allotment={$pokemonAllotment} />
+    <Recommendations />
+  </div>
+</div>
+
+<ProjectsModal />
+
+<!-- <Layout title="Editor">
   <Section id="editor_pokedex" title={`Pokédex (${$pokemon.length})`}>
     <AddPokemon />
     <Pokedex />
@@ -32,4 +54,4 @@
       </p>
     {/if}
   </Section>
-</Layout>
+</Layout> -->
