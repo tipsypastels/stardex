4058a5334f315a24dbc302fe7b2c5e31bda3c5a9
tipsypastels <dakotasankey@gmail.com>
2026-07-07 14:04:16 -0700

    headings

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,26 @@ import "./app.css";
 
 import { render } from "preact";
 import { Layout } from "./components/layout/Layout";
+import { SectionHeading } from "./components/layout/SectionHeading";
 import { Models } from "./state/context";
 
 export function App() {
   return (
     <>
       <Models>
-        <Layout />
+        <Layout
+          left={
+            <>
+              <SectionHeading title="Pokédex" />
+            </>
+          }
+          right={
+            <>
+              <SectionHeading title="Types" />
+              <SectionHeading title="Recommendations" />
+            </>
+          }
+        />
       </Models>
     </>
   );
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,29 @@
-import { Header } from "./Header";
-import { Panes } from "./Panes";
+import type { ComponentChildren } from "preact";
 
-export function Layout() {
+export interface LayoutProps {
+  left: ComponentChildren;
+  right: ComponentChildren;
+}
+
+export function Layout(props: LayoutProps) {
   return (
     <div class="flex min-h-screen flex-col">
-      <Header />
-      <Panes left={<div>hi</div>} right={<div>bye</div>} />
+      <header class="p-4">
+        <div
+          title="Stardex"
+          class="bg-primary flex h-13.75 w-13.75 items-center justify-center rounded-md text-3xl font-bold text-white shadow-xl shadow-slate-400 select-none"
+        >
+          <div>Sd</div>
+        </div>
+      </header>
+      <div class="lg:flex">
+        <Pane>{props.left}</Pane>
+        <Pane>{props.right}</Pane>
+      </div>
     </div>
   );
+}
+
+function Pane(props: { children: ComponentChildren }) {
+  return <div class="lg:grow lg:p-4">{props.children}</div>;
 }
