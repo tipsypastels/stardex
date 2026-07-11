b7b3e04b4130737280a828d904ac5f7099f92aca
tipsypastels <dakotasankey@gmail.com>
2026-07-11 08:12:59 -0700

    weird

--- src/components/common/menus/mode_picker.tsx
+++ src/components/common/menus/mode_picker.tsx
@@ -1,4 +1,3 @@
-import { For } from "@preact/signals/utils";
 import { Icon } from "../icon";
 
 interface Mode {
@@ -16,32 +15,30 @@ export interface ModePickerProps {
 export function ModePicker({ modes, activeIndex, setActiveIndex }: ModePickerProps) {
   return (
     <ul class="mb-4 last:mb-0">
-      <For each={() => modes} getKey={(mode) => mode.name}>
-        {(mode, index) => {
-          const active = index === activeIndex;
-          return (
-            <li class="border-b border-b-divider-light py-2 first:pt-0 last:border-b-0 last:pb-0">
-              <label class="flex cursor-pointer items-center border-t border-t-divider-light first:border-t-0">
-                <input
-                  class="hidden"
-                  type="radio"
-                  name="strictness"
-                  onClick={() => setActiveIndex(index)}
-                />
+      {modes.map((mode, index) => {
+        const active = index === activeIndex;
+        return (
+          <li class="border-b border-b-divider-light py-2 first:pt-0 last:border-b-0 last:pb-0">
+            <label class="flex cursor-pointer items-center border-t border-t-divider-light first:border-t-0">
+              <input
+                class="hidden"
+                type="radio"
+                name="strictness"
+                onClick={() => setActiveIndex(index)}
+              />
 
-                <div class={`mr-4 ${active ? "text-primary" : "opacity-20"}`}>
-                  <Icon name={mode.icon} />
-                </div>
+              <div class={`mr-4 ${active ? "text-primary" : "opacity-20"}`}>
+                <Icon name={mode.icon} />
+              </div>
 
-                <div>
-                  <div>{mode.name}</div>
-                  {mode.description ? <div class="text-base">{mode.description}</div> : null}
-                </div>
-              </label>
-            </li>
-          );
-        }}
-      </For>
+              <div>
+                <div>{mode.name}</div>
+                {mode.description ? <div class="text-base">{mode.description}</div> : null}
+              </div>
+            </label>
+          </li>
+        );
+      })}
     </ul>
   );
 }
