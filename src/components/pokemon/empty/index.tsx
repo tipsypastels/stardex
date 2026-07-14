99731adeb164068b6d59bf5bd171fe2e0948fe9a
tipsypastels <dakotasankey@gmail.com>
2026-07-13 19:41:29 -0700

    add text mode cta in empty state

--- src/components/pokemon/empty/index.tsx
+++ src/components/pokemon/empty/index.tsx
@@ -1,11 +1,14 @@
 import { useSignal } from "@preact/signals";
 import { Show } from "@preact/signals/utils";
+import { useContext } from "preact/hooks";
+import { PokedexModeContext } from "../../../state/context";
 import { useSaver } from "../../../state/save";
 import { ButtonLink, UploadLink } from "../../common/link";
 import { ImportPBSErrorModal, usePBSImport } from "./import_pbs";
 import { ImportRegionModal } from "./import_region";
 
 export function EmptyPokedex() {
+  const pokedexMode = useContext(PokedexModeContext);
   const importRegionModalOpen = useSignal(false);
   const saver = useSaver();
   const pbsImport = usePBSImport();
@@ -39,7 +42,7 @@ export function EmptyPokedex() {
           <li>You will be given recommendations and statistics based on your Pokédex.</li>
         </ul>
       </div>
-      <div class="rounded-b-md border-2 border-t-0 border-primary p-4">
+      <div class="mb-2 rounded-b-md border-2 border-t-0 border-primary p-4">
         <h3 class="mb-2 text-lg font-bold text-primary">Other Ways to Start</h3>
         <ul class="ml-4 list-disc">
           <li>
@@ -60,7 +63,11 @@ export function EmptyPokedex() {
         </ul>
       </div>
 
-      {/* TODO: Implement import. */}
+      <div class="text-center text-base text-foreground-lesser">
+        Don't want a visual editor? Try out{" "}
+        <ButtonLink onClick={() => (pokedexMode.key.value = "text")}>text mode</ButtonLink> for an
+        improved version of the classic Stardex experience.
+      </div>
 
       <Show when={pbsImport.error}>
         {(error) => <ImportPBSErrorModal error={error} onClose={() => pbsImport.closeError()} />}
