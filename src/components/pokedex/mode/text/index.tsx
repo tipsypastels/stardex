88b3547e90b9f213a90654e726d6d8412db89af5
tipsypastels <dakotasankey@gmail.com>
2026-07-21 00:45:54 -0700

    set the initial doc

--- src/components/pokedex/mode/text/index.tsx
+++ src/components/pokedex/mode/text/index.tsx
@@ -1,5 +1,6 @@
 import { basicSetup, EditorView } from "codemirror";
 import { onCleanup, onMount } from "solid-js";
+import { pokemons } from "../../../../models/pokemon/list";
 import { language } from "./plugins";
 import { highlightTheme, theme } from "./theme";
 
@@ -7,7 +8,9 @@ export function PokedexTextView() {
   let parent!: HTMLDivElement;
 
   onMount(() => {
+    const doc = pokemons.toSerializedText();
     const view = new EditorView({
+      doc,
       parent,
       extensions: [basicSetup, theme, language, highlightTheme],
     });
