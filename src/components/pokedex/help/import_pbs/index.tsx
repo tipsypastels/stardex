c2a6e19ad57bb6179c74dd78fc24fcb016ce58db
tipsypastels <dakotasankey@gmail.com>
2026-07-30 14:08:00 -0700

    handle empty

--- src/components/pokedex/help/import_pbs/index.tsx
+++ src/components/pokedex/help/import_pbs/index.tsx
@@ -1,10 +1,12 @@
-import type { JSXElement } from "solid-js";
+import { createSignal, type JSXElement } from "solid-js";
+import { UploadButton } from "../../../common/button";
 import { Modal } from "../../../common/menus/modal";
 import { Steps } from "../../../common/steps";
 import { ImportPBSModalLanding, ImportPBSModalLandingFooter } from "./0_landing";
 import { ImportPBSModalDexes, ImportPBSModalDexesFooter } from "./1_dexes";
 import { ImportPBSModalForms, ImportPBSModalFormsFooter } from "./2_forms";
 import { ImportPBSModalFinish, ImportPBSModalFinishFooter } from "./3_finish";
+import type { ImportPBSFiles } from "./files";
 import type { ImportPBSPhase, ImportPBSState } from "./state";
 
 export interface ImportPBSModalProps {
@@ -29,6 +31,12 @@ export function ImportPBSModal(props: ImportPBSModalProps) {
 
   function render() {
     // TODO: Handle errors and empty dexes.
+    if (props.phase.files && props.phase.files.parsed.pokemons.size === 0) {
+      return {
+        children: makeChildren(<NoPokemon />),
+        footer: <NoPokemonFooter files={props.phase.files} />,
+      };
+    }
     switch (props.phase.type) {
       case "landing": {
         return {
@@ -58,4 +66,37 @@ export function ImportPBSModal(props: ImportPBSModalProps) {
   }
 
   return <Modal title="Import PBS Files" onClose={() => props.state.close()} {...render()} />;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 No Pokémon                                 */
+/* -------------------------------------------------------------------------- */
+
+interface NoPokemonProps {
+  files: ImportPBSFiles;
+}
+
+function NoPokemon() {
+  return <>Your uploaded files have no Pokémon. Upload PBS files with at least one Pokémon.</>;
+}
+
+function NoPokemonFooter(props: NoPokemonProps) {
+  const [uploading, setUploading] = createSignal(false);
+  return (
+    <div class="flex flex-col justify-center">
+      <UploadButton
+        accept="text/plain"
+        multiple
+        disabled={uploading()}
+        // eslint-disable-next-line solid/reactivity
+        onUpload={async (fileList) => {
+          setUploading(true);
+
+          props.files.import(fileList);
+        }}
+      >
+        Upload PBS Files
+      </UploadButton>
+    </div>
+  );
 }
