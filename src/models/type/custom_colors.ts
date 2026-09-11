6480c0023c4fddf88ceb5bb828e7c11ea2e1f5e2
tipsypastels <dakotasankey@gmail.com>
2026-09-10 22:40:07 -0700

    swallow errors

--- src/models/type/custom_colors.ts
+++ src/models/type/custom_colors.ts
@@ -2,26 +2,26 @@ import { ReactiveMap } from "@solid-primitives/map";
 import { createEffect, createRoot } from "solid-js";
 import * as v from "valibot";
 import { stored } from "../../utils/storage";
-import { catchStartupError } from "../ui/error";
 
 export type RawCustomTypeColors = v.InferOutput<typeof RawCustomTypeColors>;
 export const RawCustomTypeColors = v.record(v.string(), v.string());
 
 export const customTypeColors = createRoot(() => {
-  const store = stored("stardex_custom_type_colors");
   const all = new ReactiveMap<string, string>();
-  const caught = catchStartupError("customTypeColors", () => {
+
+  try {
+    const store = stored("stardex_custom_type_colors");
     const raw_ = store.load();
-    if (!raw_) return;
-
-    const raw = v.parse(RawCustomTypeColors, raw_);
-    setFromRaw(raw);
-  });
-
-  if (!caught) {
-    createEffect(() => {
-      store.dump(toRaw());
-    });
+    if (raw_) {
+      const raw = v.parse(RawCustomTypeColors, raw_);
+      setFromRaw(raw);
+      createEffect(() => store.dump(toRaw()));
+    }
+  } catch (error) {
+    // Custom types are an extremely tertiary feature - if they're corrupted somehow,
+    // do nothing instead of saying the whole project is corrupt.
+    // eslint-disable-next-line no-console
+    console.warn("Invalid custom type colors:", error);
   }
 
   function toRaw(): RawCustomTypeColors {
